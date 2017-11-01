82e07878c7a9693dfb3061ef5e6684ee1fab5568
UFOsubject <15801399596@163.com>
2017-10-31 17:49:08 -0700

    fix

--- src/main/java/com/fly/common/logback/KafkaBuildStrategy.java
+++ src/main/java/com/fly/common/logback/KafkaBuildStrategy.java
@@ -27,7 +27,6 @@ public class KafkaBuildStrategy extends BuildStrategy{
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         MessageSender sender = KafkaSender.builder().topic(this.getConfig().getTopic()).producer(new KafkaProducer<>(props)).build();
         BuildStrategy.sender = sender;
-
         return sender;
     }
 }
